7fc5d3d01053e0cbfa95a566d4682d430bc40e3c
xiao <ilife_x@163.com>
2021-03-10 18:39:57 +0800

    个人信息编辑

---
+++ Wechat/Classes/Main/WCEditProfileViewController.h
@@ -0,0 +1,16 @@
+//
+//  WCEditProfileViewController.h
+//  Wechat
+//
+//  Created by xiao on 2021/3/10.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WCEditProfileViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Wechat/Classes/Main/WCProfileViewController.h
@@ -0,0 +1,16 @@
+//
+//  WCProfileViewController.h
+//  Wechat
+//
+//  Created by xiao on 2021/3/10.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WCProfileViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
